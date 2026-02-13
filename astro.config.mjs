89deb990606dffdf9bf63450fd7f445a8dc23ccc
Rolando <rolandomurillo@hey.com>
2026-02-13 03:05:22 -0500

    Convert to tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
+    vite: {
+        plugins: [tailwindcss()],
+    },
     site: 'https://bleep.is',
     output: 'static',
     build: {
